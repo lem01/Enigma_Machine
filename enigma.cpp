0708ecfe04d1884f5d2c30c1d9e20169ad5245cf
Desy Kristianti <dk2015@voxel11.doc.ic.ac.uk>
2015-11-11 21:39:06 +0000

    progress on class plugboard

--- enigma.cpp
+++ enigma.cpp
@@ -3,29 +3,52 @@
 |  File Name:    enigma.cpp                                                 |
 |  Student:      Desy Kristianti                                            |
 |  Coursework:   MSc C++ Programming - Assessed Exercise No. 2              |
-|  Date:         4 November 2015                                            |
+|  Date:         11 November 2015                                            |
 +--------------------------------------------------------------------------*/
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-class Mapping {
-private:
-
-public:
-  
-
-};
-
-
 class Plugboard {
 private:
-  Mapping mapping;
+  int mapping[26];
 public:
-  
+  Plugboard();
+  Plugboard(const char *filename);
+  int encrypt(int letter);
 
 };
+
+Plugboard::Plugboard() {
+  for (int i=0, i<26, i++) {
+    mapping[i]=i;
+  }
+}
+
+Plugboard::Plugboard(const char *filename) {
+  for (int i=0, i<26, i++) {
+    mapping[i]=i;
+  }
+  ifstream configure;
+  configure.open(filename);
+  configure >> number_a;
+  configure >> number_b;
+  if (number_a = number_b)
+    return 5;
+  
+
+}
+
+int Plugboard::encrypt(int letter) {
+  return mapping[letter];
+}
+
+
+
+
 
 class Rotor {
 private:
